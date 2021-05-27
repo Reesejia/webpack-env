cf34d9e20acce3ebdfc01657fc81db32ff2ac37d
wjx965558244871638129 <965558244@qq.com>
2021-05-27 11:06:35 +0800

    init vue3 webpack project

--- src/reactivity/reactive.js
+++ src/reactivity/reactive.js
@@ -1,3 +1,5 @@
+import { isObject } from "../shared/utils"
+
 export function reactive(target){
   // 创建一个响应式对象 目标对象可能不一定是数组或对象， 可能还要set map
   return createReactiveObject(target, {
@@ -14,4 +16,5 @@ function createReactiveObject(target, baseHandler){
   if(!isObject(target)){ // 不是响应式对象直接返回
     return target
   }
+  new Proxy(target, baseHandler)
 }
